42e679938dbc57b11fe5f914eeb9d2e70cf56ef3
HP <HP@DESKTOP-BAJBVHH>
2022-03-11 17:58:29 +0530

    second commit

---
+++ Day10/src/DemoVar.java
@@ -0,0 +1,19 @@
+
+public class DemoVar {
+
+	public static void main(String[] args) {
+		
+		byte num=20; //-128 to 127
+		System.out.println(num);
+		
+		short num1=5050; //-32768 to 32767
+		System.out.println(num1);
+		
+		float num2=34.67f;
+		System.out.println(num2);
+		
+
+
+	}
+
+}
