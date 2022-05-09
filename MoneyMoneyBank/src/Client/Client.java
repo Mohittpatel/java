8741c152d7358eaa33e645d02dad47bd6d4d168a
HP <HP@DESKTOP-BAJBVHH>
2022-05-09 16:20:22 +0530

    Case Study1

---
+++ MoneyMoneyBank/src/Client/Client.java
@@ -0,0 +1,18 @@
+package Client;
+
+import com.tnsif.application.*;
+import com.tnsif.framework.*;
+
+public class Client
+{
+	public static void main(String[] args) 
+	{
+		BankFactory bf = new MMBankFactory();
+		SavingAcc sa = new MMSavingAcc(12452, "mahi", 4586521, true);
+		CurrentAcc ca = new MMCurrentAcc(25754, "Mohit Patel", 45865, 500000);
+		
+		sa.withdraw(488744);
+		
+	}
+
+}
